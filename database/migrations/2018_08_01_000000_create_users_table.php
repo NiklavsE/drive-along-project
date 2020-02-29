9ff848eed6787eba57804900a851452ce4fbdd45
Niklavs Emils Eglitis <34540352+NiklavsE@users.noreply.github.com>
2020-02-29 20:07:51 +0200

    initial commit

--- database/migrations/2018_08_01_000000_create_users_table.php
+++ database/migrations/2018_08_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('surname');
             $table->string('password');
             $table->rememberToken();
         });
