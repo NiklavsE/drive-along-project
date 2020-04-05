7ca45592a00616b529fd5f07169e9bdba3901cc0
Niklavs <niklavs.emils.eglitis@gmail.com>
2020-04-05 18:12:01 +0300

    added error notifications and modals

--- resources/js/pages/Trips.js
+++ resources/js/pages/Trips.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Http from "../Http";
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -14,11 +14,26 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
 
-const useStyles = makeStyles({
+import JoinTripModal from '../components/JoinTripModal';
+
+const useStyles = theme => ({
   root: {
-    maxWidth: 200
+    minWidth: 275,
+    justify: 'center',
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
   },
 });
+
 
 class Trips extends Component {
   constructor(props) {
@@ -29,11 +44,30 @@ class Trips extends Component {
       trips: [],
       error: false,
       errorMessage: '',
+      errorId: '',
+      successId: '',
+      isJoinTripModalOpen: false,
+      joinTripModalTripId: null,
     };
 
     // API endpoint.
     this.api = "/api/v1/trips";
   }
+
+  openJoinTripModal = (tripId) => {
+    this.setState({
+      isJoinTripModalOpen: true,
+      joinTripModalTripId: tripId
+    });
+  }
+
+  closeJoinTripModal = () => {
+    this.setState({
+      isJoinTripModalOpen: false,
+      joinTripModalTripId: null
+    });
+  }
+
 
   addPassenger = (key) => {
     let updatedTrips = this.state.trips;
@@ -46,11 +80,15 @@ class Trips extends Component {
             }
           });
           this.setState({
-            trips: updatedTrips
+            trips: updatedTrips,
+            successId: response.data.success_id,
+            errorId: '',
           });
         } else {
           this.setState({
-            errorMessage: 'Trip passanger count limit reached!'
+            errorMessage: 'Trip passanger count limit reached!',
+            errorId: response.data.error_id,
+            successId: '',
           });
         }
       })
@@ -59,6 +97,7 @@ class Trips extends Component {
           error: "Sorry, there was an error joining a trip"
         });
       });
+      this.closeJoinTripModal();
   }
 
 
@@ -86,24 +125,41 @@ class Trips extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     const { trips, errorMessage } = this.state;
+
     return (
       trips.map(trip => (
-          <Card className={this.props.classes} key = {trip.id} onClick={() => this.addPassenger(trip.id)}>
-          <Alert severity="error">{this.props.errorMessage}</Alert>
-          <CardActionArea>
+          <Card className={classes.root} key = {trip.id}>
+
+          
+          { this.state.errorId == trip.id && <Alert severity="error">Pasažieru skaita limits ir sasniegts!</Alert> }
+          { this.state.successId == trip.id && <Alert severity="success">Esat veiksmīgi pievienojies braucienam! Dodaties uz "Mani braucieni" sadaļu, lai uzzinātu vairāk!</Alert> }
+
+          { this.state.joinTripModalTripId == trip.id && 
+            <JoinTripModal 
+            show={this.state.isJoinTripModalOpen} 
+            addPassenger={() => this.addPassenger(trip.id)}
+            onClose={() => this.closeJoinTripModal()}
+            />
+          }
+
             <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
+              <Typography gutterBottom variant="h5" component="h2" className={classes.title}>
                 {trip.startingPoint} - {trip.destination}
               </Typography>
-              <Typography gutterBottom variant="h4" component="h2">
+              <Typography className={classes.pos} color="textSecondary">
                 {trip.time }
               </Typography>
-              <Typography>
+              <Typography variant="body2" component="p">
               Šobrīd brīvās vietas: {trip.passangerCount}
             </Typography>
             </CardContent>
-          </CardActionArea>
+          <CardActions> 
+            <Button onClick={() => this.openJoinTripModal(trip.id)}>
+            Pieteikties braucienam
+            </Button>
+        </CardActions>
         </Card>
       ))
     );
@@ -115,4 +171,4 @@ const mapStateToProps = state => ({
   user: state.Auth.user
 });
 
-export default connect(mapStateToProps)(Trips);
+export default connect(mapStateToProps)(withStyles(useStyles)(Trips));
